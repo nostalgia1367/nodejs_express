7364a631b506a86c6c6b72101401279268f231f1
nostalgia1367 <soulbreakout@naver.com>
2021-12-27 00:58:49 +0900

    user.name가져오는 또다른 방법
    user.name가져오는 또다른 방법
    1
    2
    3

--- part1_basic/20_extract_value_from_json_object.js
+++ part1_basic/20_extract_value_from_json_object.js
@@ -1,6 +1,8 @@
 const user = { name: 'kyeongrok', age: 31 };
 console.log('user:', user);
-console.log('user.name:', user.name, 'user["name"]: ',user["name"]);
+console.log('user.name:', user.name);
+console.log('user.name가져오는 또다른 방법 user["name"]: ',user["name"]);
+
 console.log('user.age:', user.age);
 
 
